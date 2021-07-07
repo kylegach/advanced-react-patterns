811f524069d49c65d1dba9c07d0c7c3f5c9101f4
kg666261 <kyle.gach@broadcom.com>
2021-07-07 14:23:26 -0600

    Complete 04.extra-1

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,20 +4,42 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn?.(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  return {on, toggle, togglerProps: {'aria-pressed': on, onClick: toggle}}
+  function getTogglerProps(togglerProps) {
+    return {
+      'aria-pressed': on,
+      ...togglerProps,
+      onClick: callAll(toggle, togglerProps?.onClick),
+    }
+  }
+
+  return {on, toggle, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
